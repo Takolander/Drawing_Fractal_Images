e574f9f43e0da753dce851fe8bbd3c2fa351c665
William Takolander <william_tako@hotmail.com>
2020-08-14 15:02:45 +0200

    Bitmap
    Have created a bit map where you can set individual pixels to any RGB you like. This includes a Bitmap class and a fileheader and a infoheader.

---
+++ Fractal_images/Fractal_images/BitmapFileHeader.h
@@ -0,0 +1,14 @@
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+#include <cstdint>
+#pragma pack(push, 2)
+
+struct BitmapFileHeader {
+	char header[2]{ 'B', 'M' };
+	int32_t fileSize;
+	int32_t reserved{ 0 };
+	int32_t dataOffset;
+};
+
+#pragma pack(pop)
+#endif
---
+++ Fractal_images/Fractal_images/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Bitmap.h"
+
+int main() {
+
+	int const WIDTH = 800;
+	int const HEIGHT = 600;
+	Bitmap bitmap(WIDTH, HEIGHT);
+
+	for (int y = 0; y < HEIGHT; y++)
+	{
+		for (int x = 0; x < WIDTH; x++)
+		{
+			bitmap.setPixel(x, y, 255, 0, 0);
+		}
+	}
+
+	bitmap.write("test.bmp");
+
+	std::cout << "Finished." << std::endl;
+	return 0;
+}
